a2569b19fa6c4569e950a4618202a72ca467ac9a
Arman <77539286+Armanghani@users.noreply.github.com>
2026-08-21 17:16:23 +0330

    Delete contact.js

--- contact.js
+++
@@ -1,5 +0,0 @@
-const contactForm = document.getElementById('contactForm');
-contactForm?.addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('Thank you. Your enquiry is ready to be connected to the VENICE contact endpoint.');
-});
